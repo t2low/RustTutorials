cb739caa61aa994c151072f23b375b249ab97202
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-07-09 02:10:42 +0900

    loop while for Range rev を試す

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -26,4 +26,26 @@ fn main() {
         6
     };
     println!("The value of number is: {}", number);
+
+    //     loop {
+    //         println!("again!");
+    //     }
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}", number);
+        number = number - 1;
+    }
+    println!("LIFTOFF!!!");
+
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
