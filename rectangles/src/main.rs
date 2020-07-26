f84469bc447560bc2c9875bca1afadc5249a39e1
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-07-27 02:00:07 +0900

    関連関数(staticのような？)を定義

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -27,5 +31,11 @@ fn main() {
     let rect3 = Rectangle { width: 60, height: 45 };
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!(
+            "The area of the square is {} square pixels.",
+            sq.area()
+    );
 }
 
